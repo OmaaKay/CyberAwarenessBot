bca6fc1638b74b2bf654ff0040a23a34f3ed63ad
OmaaKay <matladigitall@gmail.com>
2026-04-08 23:49:20 +0200

    added a voice greeting using wav at the start oftheapplication

--- obj/Debug/net9.0/CyberAwarenessBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/CyberAwarenessBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f307b2c14e353be2e7c8c8963be9f1ef6c504671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cc13e6552bb6075261337135e3154d8a70e205")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberAwarenessBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
